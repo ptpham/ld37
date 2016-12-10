6d72a101c17b4b4d294c3376d1a8281ff8c87e19
Peter Pham <ptr.pham@gmail.com>
2016-12-10 09:45:56 -0800

    Adding dependencies.

---
+++ deps.js
@@ -0,0 +1,13 @@
+
+glMatrix = require('gl-matrix');
+v2 = glMatrix.vec2;
+v3 = glMatrix.vec3;
+m2 = glMatrix.mat2;
+m3 = glMatrix.mat3;
+m4 = glMatrix.mat4;
+
+createVAO = require('gl-vao');
+createFBO = require('gl-fbo');
+createShader = require('gl-shader');
+createTexture = require('gl-texture2d');
+
